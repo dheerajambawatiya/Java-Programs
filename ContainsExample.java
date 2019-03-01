c409eed33e1c94f332385672a168d3e5e9304482
dheeajambawatiya <47988975+dheeajambawatiya@users.noreply.github.com>
2019-03-01 21:19:56 +0530

    Add files via upload

---
+++ ContainsExample.java
@@ -0,0 +1,7 @@
+class ContainsExample{  
+public static void main(String args[]){  
+String name="what do you know about me";  
+System.out.println(name.contains("do you know"));  
+System.out.println(name.contains("about you"));  
+System.out.println(name.contains("hello"));  
+}}  
