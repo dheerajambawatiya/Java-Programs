ded4a974611a60f031d936d779d90fd477022a72
dheeajambawatiya <47988975+dheeajambawatiya@users.noreply.github.com>
2019-03-01 21:19:26 +0530

    Add files via upload

---
+++ ConcatExample.java
@@ -0,0 +1,7 @@
+public class ConcatExample{  
+public static void main(String args[]){  
+String s1="java string";
+String s2="is fair";        
+s1=s1.concat(s2);  
+System.out.println(s1);  
+}}  
