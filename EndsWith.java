33890fcc5013cd4a45091297ddce58c6c04cabd1
dheeajambawatiya <47988975+dheeajambawatiya@users.noreply.github.com>
2019-03-01 21:22:15 +0530

    Add files via upload

---
+++ EndsWith.java
@@ -0,0 +1,7 @@
+public class EndsWith{  
+public static void main(String args[]){  
+String s1="java by javatpoint";  
+System.out.println(s1.endsWith("t"));  
+System.out.println(s1.endsWith("point"));
+System.out.println(s1.endsWith("java"));   
+}}  
