7309d6fd4a6368f9f292552eac1da502a7025b7b
dheeajambawatiya <47988975+dheeajambawatiya@users.noreply.github.com>
2019-03-11 13:13:42 +0530

    Update Pattern3.java

--- Pattern3.java
+++ Pattern3.java
@@ -8,3 +8,16 @@ class Pattern3 {
 		}
 	}
 }
+
+// or
+
+class Pattern3 {
+	public static void main(String args[]) {
+		for(int i=5;i>=1;i--) {
+			for (int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
