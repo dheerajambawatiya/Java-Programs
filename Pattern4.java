9b0c4a30c32b7415d0685f75872320dfba94de5c
dheeajambawatiya <47988975+dheeajambawatiya@users.noreply.github.com>
2019-03-11 13:35:10 +0530

    Add files via upload

---
+++ Pattern4.java
@@ -0,0 +1,15 @@
+class Pattern4 {
+	public static void main(String args[]) {
+		int n =5; // Size
+		for(int i=1;i<=n;i++) {
+			for (int j=n-1;j>=i;j--) {
+				System.out.print(" ");
+			}
+			for (int k=1;k<=i;k++)
+			{
+				System.out.print("*");
+				}
+			System.out.println();
+		}
+	}
+}
