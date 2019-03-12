2f5b68647c981417d853cd61f906cefd4ed59cc5
dheeajambawatiya <47988975+dheeajambawatiya@users.noreply.github.com>
2019-03-13 00:22:11 +0530

    Add files via upload

---
+++ Pattern6.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Pattern6 {
+	public static void main(String args[]) {
+		int n;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Value");
+		n = sc.nextInt();
+		System.out.println();
+		for(int i=1;i<=n;i++) {
+			for (int j=1;j<=i;j++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+}
