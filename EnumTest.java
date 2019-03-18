fca4c30bf82a6d20a8f12bfa7960d257d159721d
dheeajambawatiya <47988975+dheeajambawatiya@users.noreply.github.com>
2019-03-19 00:07:39 +0530

    Add files via upload

---
+++ EnumTest.java
@@ -0,0 +1,28 @@
+class EnumTest {
+	enum Books{MyBook , HisBook , YourBook};
+	public static void main(String args[]) {
+		Books b = Books.MyBook;
+		if(b.equals(Books.MyBook))
+			System.out.println("This is my book");
+		else
+			
+			System.out.println("This is not my book");
+		if(b == Books.MyBook)
+			
+			System.out.println("This is my book");
+		else
+			
+			System.out.println("This is not my book");
+		switch(b) {
+			case MyBook:
+				System.out.println("MyBook");
+			break;
+			case HisBook:	
+				System.out.println("HisBook");
+			break;
+			case YourBook:
+				System.out.println("YourBook");
+			break;
+		}
+	}
+}
