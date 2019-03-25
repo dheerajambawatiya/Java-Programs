e415954cb0282112f4485735f704c5429f9e97bd
dheeajambawatiya <47988975+dheeajambawatiya@users.noreply.github.com>
2019-03-25 18:15:13 +0530

    Update ArraySum.java

--- ArraySum.java
+++ ArraySum.java
@@ -3,9 +3,9 @@ public static void main(String[] args) {
        int[] numbers = new int[]{20, 30, 25, 35, -16, 60, -100};
        int sum = 0;
        for(int i=0; i < numbers.length ; i++)
-        sum = sum + numbers[i];
-        double average = sum / numbers.length;
+        sum = sum + numbers[i];                          // sum += numbers[i]
+     // for average   double average = sum / numbers.length;
 	System.out.println("Sum is :" +sum);
-        System.out.println("Average value of the array elements is : " + average); 
+        // System.out.println("Average value of the array elements is : " + average); 
    }
 }
