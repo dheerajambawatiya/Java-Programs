d1d5f3abc87fc072d6962039300097153616ba38
dheeajambawatiya <47988975+dheeajambawatiya@users.noreply.github.com>
2019-04-02 00:50:48 +0530

    Add files via upload

---
+++ BlockLambda.java
@@ -0,0 +1,15 @@
+interface NumericFunc {
+	int func(int n);
+}
+class BlockLambda {
+	public static void main(String args[]) {
+		NumericFunc factorial = n -> {     // this block lambda computes the factorial of an int value.
+ 			int result = 1;
+			for (int i=1 ; i<=n ; i++)
+				result += i*result;
+			return result;
+		};
+		System.out.println("The factorial of 3 is :" +factorial.func(3));
+		System.out.println("The factorial of 5 is :" +factorial.func(5));
+	}
+}
